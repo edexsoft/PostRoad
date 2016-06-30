506aab0cd7317418ebeb70d12f58852b6b18305c
matrix.static <matrix.static@gmail.com>
2016-06-30 12:02:28 +0800

    init

---
+++ src/postroad.domain.implemant/src/main/java/com/edexsoft/postroad/domain/implemant/App.java
@@ -0,0 +1,13 @@
+//package com.edexsoft.postroad.domain.implemant;
+//
+///**
+// * Hello world!
+// *
+// */
+//public class App 
+//{
+//    public static void main( String[] args )
+//    {
+//        System.out.println( "Hello World!" );
+//    }
+//}
---
+++ src/postroad.domain.repository/src/main/java/com/edexsoft/postroad/domain/repository/App.java
@@ -0,0 +1,13 @@
+package com.edexsoft.postroad.domain.repository;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
